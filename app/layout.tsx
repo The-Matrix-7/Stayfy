97e8a60fa7bbdb618c511e7e08cc55ecddd919d1
Daniel Guo <guodaniel1@gmail.com>
2023-08-18 19:01:22 -0700

    Modal & Buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from 'next/font/google'
 
 import './globals.css'
 import Navbar from './components/navbar/Navbar';
+import Modal from './components/modals/Modal';
 
 export const metadata = {
   title: 'Stayfy',
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal actionLabel="Submit" title="Hello World" isOpen />
         <Navbar />
         {children}
       </body>
