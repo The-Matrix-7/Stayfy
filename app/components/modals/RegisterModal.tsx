6f2494cd2052de4bc7e215a587a54ae776686de1
Daniel Guo <guodaniel1@gmail.com>
2023-08-23 22:31:05 -0700

    Github Google login

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -16,6 +16,7 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import Button from '../Button';
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -92,13 +93,13 @@ const RegisterModal = () => {
         outline
         label="Continue with Google" 
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn('google')}
       />
       <Button 
         outline
         label="Continue with Github" 
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github')}
       />
       <div
         className="
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,9 +5,11 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+//import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
   currentUser?: User | null;
+  //currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({
