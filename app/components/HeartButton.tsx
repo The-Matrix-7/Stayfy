27e00a19b5fa396fe9bd8fdad901bb0662afecf8
Daniel Guo <guodaniel1@gmail.com>
2023-09-01 00:54:12 -0600

    Favorite functionality

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { User } from "@prisma/client";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps {
   listingId: string;
@@ -12,9 +13,11 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser
 }) => {
-  const hasFavorited = false;
-  const toggleFavorite = () => {};
-
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
+  
   return ( 
     <div
       onClick={toggleFavorite}
